5fb6dbc2c74d70d0729cb8861913f5abecd67ffb
ahmad <ahmadquraan.git@gmail.com>
2026-07-25 18:36:58 +0300

    Add nginx config, script && Dockerfile

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-mkdir /etc/nginx/ssl
+# IF anything failed, non-zero exit code
+set -e 
 
-if [ ! /etc/nginx/ssl/fullchain.pem ]; then
+mkdir -p /etc/nginx/ssl
+
+if [ ! -f /etc/nginx/ssl/public.pem ]; then
+	# -nodes: TO avoid entring password for private key
+	    # -subj avoids interactive prompts for identity fields
 	openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-		# PRivate key
-        -keyout /etc/nginx/ssl/privkey.pem \
-		# Public key
-        -out /etc/nginx/ssl/fullchain.pem \
-
-		# To avoid openssl to ask questions
-        -subj "/C=FR/ST=Paris/L=Paris/O=42/OU=Inception/CN=${DOMAIN_NAME}"
+        -keyout /etc/nginx/ssl/private.pem \
+        -out /etc/nginx/ssl/public.pem \
+        -subj "/C=JO/ST=Amman/L=Amman/O=42/OU=42/CN=${DOMAIN_NAME}"
 
 fi
 
 
+# Nginx can't read env variables directly
+# Substitue {DOMAIN NAME} Before starting nginx.conf
+if  ! id nginx >/dev/null 2>&1 ; then 
+	groupadd -r nginx
+	useradd -r -g nginx nginx 
+fi
+
+envsubst '${DOMAIN_NAME}' < /tmp/nginx.conf  > /etc/nginx/nginx.conf
 exec nginx -g "daemon off;"
 
